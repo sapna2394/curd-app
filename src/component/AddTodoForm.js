e14687ff6b3f3128362a9faf10348bba485928e5
Your Name <sapnapandey20@navgurukul.org>
2022-03-31 12:21:33 +0530

    modify something

--- src/component/AddTodoForm.js
+++ src/component/AddTodoForm.js
@@ -1,18 +1,20 @@
 function AddTodoForm({
-    todo,
-    onAddFormSubmit,
-    onAddInputChange
-  }) {
-    return (
+  todo,
+  onAddFormSubmit,
+  onAddInputChange
+}) {
+  return (
+    <>
+      <h2 className="head">TODO LIST</h2>
       <form onSubmit={onAddFormSubmit}>
-        <h2 className="head">TODO LIST</h2>
         <label htmlFor="todo"><i>CREATE TODO: </i></label>
         <input
-          name="todo"type="text"placeholder="Create new todo"
+          name="todo" type="text" placeholder="Create new todo"
           value={todo}
           onChange={onAddInputChange}
         />
       </form>
-    );
-  }   
+    </>
+  );
+}
 export default AddTodoForm
